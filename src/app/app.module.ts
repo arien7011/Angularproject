1c579a7c4937e7391ca00f95e6014a7ad92553c2
Aman <aman.mishra@taazaa.com>
2022-09-04 01:06:48 +0530

    Revert "added some files"
    This reverts commit 115f807e52f6b66e9008416e903181e8ab4f4864.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule,routingComponents } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TabsModule} from 'ngx-bootstrap/tabs';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { Ng2SearchPipeModule } from 'ng2-search-filter';
-import { OrderModule } from 'ngx-order-pipe';
-import { EmployeeService } from './employee.service';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { PostPaginationComponent } from './post-pagination/post-pagination.component';
-import {NgxPaginationModule} from 'ngx-pagination';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    routingComponents,
-    PageNotFoundComponent,
-    PostPaginationComponent
-    
+    AppComponent
   ],
- 
   imports: [
     BrowserModule,
-    AppRoutingModule,BrowserAnimationsModule,FormsModule,ReactiveFormsModule,HttpClientModule,
-    TabsModule.forRoot(),Ng2SearchPipeModule,NgxPaginationModule,OrderModule,NgbModule
+    AppRoutingModule
   ],
-
-  providers: [EmployeeService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/practice/Profile.ts
+++
@@ -1,9 +0,0 @@
-
-
-export class profile{
-    name:string;
-    state:string;
-    email:string;
-    phone:string
-}
-
